08fbfd0251901fe0cf2ba0855dc6de731a3f4265
johnbeto20 <johnalberto.sanchez99@gmail.com>
2026-07-28 13:33:58 -0500

    actualización portafolio

---
+++ js/projects-data.js
@@ -0,0 +1,273 @@
+// Archivo generado automaticamente por generate_projects.py
+// No editar a mano: los cambios se pierden al volver a ejecutar el script.
+const PROJECT_CATEGORIES = [
+  {
+    "slug": "ilustraciones",
+    "label": "Ilustraciones"
+  },
+  {
+    "slug": "tomas-aereas",
+    "label": "Tomas Aereas"
+  },
+  {
+    "slug": "aplicaciones-y-sitios-web",
+    "label": "Aplicaciones y Sitios Web"
+  },
+  {
+    "slug": "animaciones",
+    "label": "Animaciones"
+  },
+  {
+    "slug": "disenio-grafico",
+    "label": "Diseño Grafico"
+  }
+];
+const PROJECTS = [
+  {
+    "slug": "Renting Colombia",
+    "title": "Renting Colombia",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Renting Colombia / Localiza: cambio de marca y estándar gráfico, además de acompañamiento visual en firma electrónica, devoluciones de datos bancarios y alianza retail de usados. (2021 - 2024, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/Renting Colombia/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/Renting Colombia/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "EPM",
+    "title": "EPM",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Identidad y lineamientos UI: unificación de estilo gráfico y diseño de elementos visuales para los proyectos de EPM. (2020 - actualidad, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/EPM/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/EPM/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "XM",
+    "title": "XM",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Portal de recursos digitales y componentes empresariales para el equipo de desarrollo de XM, con orientación visual de marca. (2023 - 2025, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/XM/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/XM/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "grupoSura",
+    "title": "Grupo Sura",
+    "category": "ilustraciones",
+    "categoryLabel": "Ilustraciones",
+    "description": "Grupo SURA - Portal de Procesos Cobit: propuesta gráfica para la aplicación web del producto y acompañamiento en el entendimiento del negocio. (2017 - 2018)",
+    "url": null,
+    "image": "img/ilustraciones/grupoSura/banner-grupoSura-portal-proceso.jpg",
+    "images": [
+      "img/ilustraciones/grupoSura/banner-grupoSura-portal-proceso.jpg",
+      "img/ilustraciones/grupoSura/boceto-portalproceso-banner 05032018.jpg"
+    ]
+  },
+  {
+    "slug": "SIMPLEX",
+    "title": "SIMPLEX",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Simplex - XM: construcción de la línea gráfica de la interfaz y estandarización de la unidad gráfica del sistema de generación y gestión de casos. (2019 - actualidad, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/SIMPLEX/00.dashboard-crear-caso-menu-hide.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/SIMPLEX/00.dashboard-crear-caso-menu-hide.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/01.dashboard-crear-caso.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/02.dashboard-parametrizacion.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/03.dashboard-informacion-de-estudio.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/04.dashboard-informacion-de-estudio-carga-masiva.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/05.dashboard-generar-caso-estudio.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/06.dashboard-generar-caso-estudio-tooltip.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/07.dashboard-generar-caso-estudio-detalle.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/08.dashboard-modal-1.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/09.dashboard-modal-2.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/10.dashboard-opciones-configuracion.jpg",
+      "img/aplicaciones-y-sitios-web/SIMPLEX/10.iconografia y logo.jpg"
+    ]
+  },
+  {
+    "slug": "EPM-AtencionDigital",
+    "title": "EPM Atencion Digital",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Atención de marca digital para EPM desde una agencia digital: mantenimiento del sitio web corporativo, piezas para redes sociales, banners y campañas publicitarias. (2016 - 2018, Arkix S.A. / Julius Group)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/EPM-AtencionDigital/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/EPM-AtencionDigital/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "DeffineStaffing",
+    "title": "Deffine Staffing",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Revisión de la implementación gráfica de las pantallas de la interfaz, proponiendo elementos interactivos alineados a la propuesta de experiencia de usuario. (2022 - 2023, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/DeffineStaffing/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/DeffineStaffing/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "Comfamiliar",
+    "title": "Comfamiliar",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Apoyo al equipo de desarrollo en la unidad gráfica de la interfaz, revisando buenas prácticas en la construcción del HTML y demás elementos del frontend. (2023, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/Comfamiliar/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/Comfamiliar/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "Siigo",
+    "title": "Siigo",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "Sistema de diseño para Siigo: creación, organización y tokenización de componentes en un UI Kit útil para los equipos de diseño y desarrollo. (2026 - actualidad, vía Ceiba Software)",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/Siigo/placeholder.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/Siigo/placeholder.jpg"
+    ]
+  },
+  {
+    "slug": "ceibaSoftware",
+    "title": "Ceiba Software",
+    "category": "disenio-grafico",
+    "categoryLabel": "Diseño Grafico",
+    "description": "",
+    "url": null,
+    "image": "img/disenio-grafico/ceibaSoftware/ceiba-software-generandoEstrategiasUX.jpg",
+    "images": [
+      "img/disenio-grafico/ceibaSoftware/ceiba-software-generandoEstrategiasUX.jpg"
+    ]
+  },
+  {
+    "slug": "ceibaSoftware",
+    "title": "Ceiba Software",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/ceibaSoftware/sitio-web-ceiba-en-ingles.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/ceibaSoftware/sitio-web-ceiba-en-ingles.jpg"
+    ]
+  },
+  {
+    "slug": "MAIA",
+    "title": "MAIA",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/MAIA/arbol v2.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/MAIA/arbol v2.jpg",
+      "img/aplicaciones-y-sitios-web/MAIA/dashboard.jpg",
+      "img/aplicaciones-y-sitios-web/MAIA/image-tree-resolution.png",
+      "img/aplicaciones-y-sitios-web/MAIA/Resoluciones-porEstado.jpg"
+    ]
+  },
+  {
+    "slug": "DanielaWeddings",
+    "title": "Daniela Weddings",
+    "category": "ilustraciones",
+    "categoryLabel": "Ilustraciones",
+    "description": "",
+    "url": "https://danielahenao.com/adecadeasart/",
+    "image": "img/ilustraciones/DanielaWeddings/Image simple/2026-02-13_154641.jpg",
+    "images": [
+      "img/ilustraciones/DanielaWeddings/Image simple/2026-02-13_154641.jpg",
+      "img/ilustraciones/DanielaWeddings/Image simple/image-simple-transparent-generative.png",
+      "img/ilustraciones/DanielaWeddings/Image simple/image-simple-transparent.png",
+      "img/ilustraciones/DanielaWeddings/Image simple/image-simple.png",
+      "img/ilustraciones/DanielaWeddings/image-big/Document_2026-02-13_065330.jpg",
+      "img/ilustraciones/DanielaWeddings/image-big/image-BIG.png"
+    ]
+  },
+  {
+    "slug": "virtualHeald",
+    "title": "Virtual Heald",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/virtualHeald/00.index.png",
+    "images": [
+      "img/aplicaciones-y-sitios-web/virtualHeald/00.index.png",
+      "img/aplicaciones-y-sitios-web/virtualHeald/01.perfil.png",
+      "img/aplicaciones-y-sitios-web/virtualHeald/02.historia.png",
+      "img/aplicaciones-y-sitios-web/virtualHeald/03.interes.png",
+      "img/aplicaciones-y-sitios-web/virtualHeald/04.programas.png"
+    ]
+  },
+  {
+    "slug": "portadas",
+    "title": "Portadas",
+    "category": "ilustraciones",
+    "categoryLabel": "Ilustraciones",
+    "description": "",
+    "url": null,
+    "image": "img/ilustraciones/portadas/backgroundDefault 2.jpg",
+    "images": [
+      "img/ilustraciones/portadas/backgroundDefault 2.jpg",
+      "img/ilustraciones/portadas/backgroundDefault 3.jpg",
+      "img/ilustraciones/portadas/backgroundDefault 4.jpg",
+      "img/ilustraciones/portadas/backgroundDefault.jpg",
+      "img/ilustraciones/portadas/ceiba15.jpg",
+      "img/ilustraciones/portadas/gráfico-portadas.jpg"
+    ]
+  },
+  {
+    "slug": "convocatorias",
+    "title": "Convocatorias",
+    "category": "ilustraciones",
+    "categoryLabel": "Ilustraciones",
+    "description": "",
+    "url": null,
+    "image": "img/ilustraciones/convocatorias/01.Q2GestionHumanaConvocatorias21052019.jpg",
+    "images": [
+      "img/ilustraciones/convocatorias/01.Q2GestionHumanaConvocatorias21052019.jpg",
+      "img/ilustraciones/convocatorias/02Q3GestionHumanaConvocatorias20022019.png",
+      "img/ilustraciones/convocatorias/04Q4GestionHumanaConvocatorias12032019.png",
+      "img/ilustraciones/convocatorias/05Q3GestionHumanaConvocatorias25022019.png",
+      "img/ilustraciones/convocatorias/Convocatoria-CoachTécnicoIOS-CeibaSoftware-facebook.png",
+      "img/ilustraciones/convocatorias/Convocatoria-CoachTécnicoJava-CeibaSoftware-facebook.png",
+      "img/ilustraciones/convocatorias/Convocatoria-Desarrolladores-Golang- facebook.png",
+      "img/ilustraciones/convocatorias/Convocatoria-DevOpsAutomation-CeibaSoftware-Facebook.jpg",
+      "img/ilustraciones/convocatorias/facebook.png",
+      "img/ilustraciones/convocatorias/Vacante-Desarrolladores-Phyton-facebook.jpg"
+    ]
+  },
+  {
+    "slug": "GCA",
+    "title": "GCA",
+    "category": "aplicaciones-y-sitios-web",
+    "categoryLabel": "Aplicaciones y Sitios Web",
+    "description": "",
+    "url": null,
+    "image": "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-Home-GCA.jpg",
+    "images": [
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-Home-GCA.jpg",
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-Home-submenu-GCA.jpg",
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-interna-GCA.jpg",
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-itinerarios-GCA.jpg",
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-nuestraCompania-GCA-09292018.jpg",
+      "img/aplicaciones-y-sitios-web/GCA/propuestaGrafica-politicasDeEquipaje-GCA.jpg"
+    ]
+  }
+];
